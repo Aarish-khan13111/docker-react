a7a9254a821b618262b81cb459f79cb642610423
Aarish-khan13111 <aarishkhan13111@gmail.com>
2024-05-28 18:41:46 +0530

    code fix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          <h1>Hello sir !!</h1>
+          <p>Hello sir !!</p>
         </p>
         <a
           className="App-link"
